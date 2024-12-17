546c09d0221cc9a4aa6433967eed024fe27c8575
IfeJayeola <87848709+Ifejaye@users.noreply.github.com>
2024-12-17 13:42:57 +0100

    Update post.ts

--- models/post.ts
+++ models/post.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const postSchema = new mongoose.Schema({
-    input1: {type: String, required: true},
-    input2: {type: String, required: true},
+    contentType: {type: String, required: true},
+    content: {type: String, required: true},
     location: {type: String, required: false},
     // date: {type: Date, default: Date.now} 
 }, {timestamps: true})
